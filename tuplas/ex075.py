e861a1eb0908a4c722d8f223d55cb81704b4fb8a
Igo Medeiros <igopdm@gmail.com>
2018-08-06 16:57:40 -0300

    Remove unnecessary variables.

--- tuplas/ex075.py
+++ tuplas/ex075.py
@@ -7,22 +7,16 @@ print("~="*20)
 
 numbersName = ("primeiro", "segundo", "terceiro", "último")
 numbers = tuple(int(input(f"Entre com o {numbersName[i]} numeros: ")) for i in range(4))
-appearNumber9 = 0
-positionNumber3 = 0
 pairNumbers = []
 
-for pos, number in enumerate(numbers):
-    if number == 9:
-        appearNumber9 += 1
-    if number == 3 and positionNumber3 <= 0:
-        positionNumber3 = pos+1
+for number in numbers:
     if number % 2 == 0:
         pairNumbers.append(number)
 
-if appearNumber9:
-    print(f"O número 9 apareceu {appearNumber9} vezes.")
-if positionNumber3:
-    print(f"O primeiro valor 3 apareceu na posição {positionNumber3}.")
+if numbers.count(9):
+    print(f"O número 9 apareceu {numbers.count(9)} vezes.")
+if 3 in numbers:
+    print(f"O primeiro valor 3 apareceu na posição {numbers.index(3)}.")
 if pairNumbers:
     print(f"Os números pares foram {pairNumbers}.")
 
