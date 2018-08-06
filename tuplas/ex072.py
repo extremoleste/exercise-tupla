58aba0b452ef7d28555e0a0062213c20f3e0ede8
Igo Medeiros <igomedeiros@MacBook-Pro-de-Igo.local>
2018-08-06 07:14:15 -0300

    Solve ex072

---
+++ tuplas/ex072.py
@@ -0,0 +1,10 @@
+print('~='*20)
+print('{:^20}'.format('Numeral para Extenso'))
+print('~='*20)
+numbers = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Duatorze', 'Duinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+while True:
+	userChoice = int(input('Escolha um número entre 0 e 20: '))
+	if userChoice >= 0 and userChoice <=20:
+		break
+print(f'O número {userChoice} por extenso é {numbers[userChoice]}')
+print('~='*20)
